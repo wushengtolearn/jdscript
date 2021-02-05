6239b6afc90ccfacf6b488abf783f7af6272dcd7
wushengtolearn <64878589+wushengtolearn@users.noreply.github.com>
2021-02-05 08:36:26 +0800

    Update jd_bean_sign.js

--- jd_bean_sign.js
+++ jd_bean_sign.js
@@ -52,7 +52,7 @@ if ($.isNode()) {
 async function execSign() {
   console.log(`\n开始执行脚本签到，请稍等`)
   try {
-    if (notify.SCKEY || notify.BARK_PUSH || notify.DD_BOT_TOKEN || (notify.TG_BOT_TOKEN && notify.TG_USER_ID) || notify.IGOT_PUSH_KEY) {
+    if (notify.SCKEY || notify.BARK_PUSH || notify.DD_BOT_TOKEN || (notify.TG_BOT_TOKEN && notify.TG_USER_ID) || notify.PUSH_KEY) {
       await exec("node JD_DailyBonus.js >> result.txt");
     } else {
       // 如果没有提供通知推送，则打印日志
