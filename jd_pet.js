58e7d0b2f7f45f51a28fa981692eec7fa7151e58
wushengtolearn <64878589+wushengtolearn@users.noreply.github.com>
2021-04-02 15:12:21 +0800

    Update jd_pet.js

--- jd_pet.js
+++ jd_pet.js
@@ -30,12 +30,12 @@ let cookiesArr = [], cookie = '', jdPetShareArr = [], isBox = false, notify, new
 //下面给出两个账号的填写示例（iOS只支持2个京东账号）
 let shareCodes = [ // IOS本地脚本用户这个列表填入你要助力的好友的shareCode
    //账号一的好友shareCode,不同好友的shareCode中间用@符号隔开
-  'MTAxODc2NTEzNTAwMDAwMDAwMjg3MDg2MA==@MTAxODc2NTEzMzAwMDAwMDAyNzUwMDA4MQ==@MTAxODc2NTEzMjAwMDAwMDAzMDI3MTMyOQ==@MTAxODc2NTEzNDAwMDAwMDAzMDI2MDI4MQ==@MTAxODcxOTI2NTAwMDAwMDAxOTQ3MjkzMw==',
+  'MTAxODc2NTE0NzAwMDAwMDAyNTE3ODU5MQ==@MTAxODExNDYxMTEwMDAwMDAwMzkyOTI1NzE=@MTAxODc2NTEzNDAwMDAwMDAyODMwNjcwOQ==',
   //账号二的好友shareCode,不同好友的shareCode中间用@符号隔开
-  'MTAxODc2NTEzMjAwMDAwMDAzMDI3MTMyOQ==@MTAxODcxOTI2NTAwMDAwMDAyNjA4ODQyMQ==@MTAxODc2NTEzOTAwMDAwMDAyNzE2MDY2NQ==',
+  'MTAxODc2NTEzOTAwMDAwMDAyMjU5MzA5OQ==@MTAxODc2NTE0NzAwMDAwMDAyNTE3ODU5MQ==',
 ]
 let message = '', subTitle = '', option = {};
-let jdNotify = true;//是否关闭通知，false打开通知推送，true关闭通知推送
+let jdNotify = false;//是否关闭通知，false打开通知推送，true关闭通知推送
 const JD_API_HOST = 'https://api.m.jd.com/client.action';
 let goodsUrl = '', taskInfoKey = [];
 let randomCount = 20;
